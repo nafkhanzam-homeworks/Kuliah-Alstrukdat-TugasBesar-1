be1798276b0d34d30148d74c687d1c1357cb81c2
falahsutawindaya-coder <54474247+falahsutawindaya-coder@users.noreply.github.com>
2019-11-25 01:09:52 +0700

    komen owow

--- src/adt/mesinkata.h
+++ src/adt/mesinkata.h
@@ -10,12 +10,28 @@ typedef struct _MesinKata {
 } MesinKata;
 
 MesinKata new_MesinKata();
+/*I.S struktur data mesin kata tidak terdefinisi
+F.S  mesin kata baru terdefinisi*/
 MesinKata new_FileMesinKata(char* fileName);
+/*I.S file eksternal tidak terdefinisi dan filename terdefinisi
+F.S file eksternal baru terdefinisi dengan  filename sebagai title*/
 void MesinKata_startFile(MesinKata* p, char* fileName);
+/*I.S file eksternal mesin kata terdefinisi
+F.S  membaca mesin kata pada file eksternal siap untuk di proses (di manipulasi stringnya)*/
 void MesinKata_ignoreBlank(MesinKata* p);
+/*I.S mesin kata terdefinisi
+F.S pembacaan mesin kata dengan blank di hapus*/
 void MesinKata_adv(MesinKata* p);
+/*I.S mesin kata terdefinisi
+F.S  mesin kata di baca selanjutnya (next(mesinkata))*/
 char* MesinKata_readString(MesinKata* p);
+/*I.S mesin kata terdefinisi
+F.S  membaca string/return string pada posisi relatif adv*/
 int MesinKata_readInt(MesinKata* p);
+/*I.S mesin kata terdefinisi
+F.S  mesin kata dengan integer di proses/di return pada posisi relatif adv*/
 boolean MesinKata_isNonReadableCharacter(MesinKata* p);
+/*I.S mesin kata terdefinisi 
+F.S  return boolean karakter yang bukan string biasa atau integer*/
 
 #endif // __mesinkata_h__
