3d143968bc9302e259b5b56ff05259dd73346e09
falahsutawindaya-coder <54474247+falahsutawindaya-coder@users.noreply.github.com>
2019-11-25 00:58:37 +0700

    komen

--- src/adt/queue.h
+++ src/adt/queue.h
@@ -8,12 +8,25 @@ typedef struct _Queue {
 } Queue;
 
 Queue new_Queue();
+/*I.S queue tidak terdefinisi
+F.S queue baru terdefinisi*/
 boolean Queue_isEmpty(Queue* p);
+/*I.S queue terdefinisi
+F.S kondisi boolean ketika queue kosong*/
 int Queue_getLength(Queue* p);
+/*I.S queue terdefinisi
+F.s mendapatkan panjang queue*/
 void Queue_add(Queue* p, int v);
+/*I.S queue terdefinisi dan v terdefinisi
+ F.S menambahkan elemen v ke queue */
 int Queue_remove(Queue* p);
+/*I.S queue terdefinisi
+F.S menghapus queue atau dealokasi queue*/
 int Queue_peek(Queue* p);
+/*I.S queue terdefinisi
+F.S melihat info queue*/
 void Queue_clear(Queue* p);
-
+/*I.S queue terdefinisi
+F.S mengosongkan queue tp tidak di dealokasi*/
 #endif // __queue_h__
 
