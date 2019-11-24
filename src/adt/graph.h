943488308b011d76a40a09e3c0261e9808e34de4
falahsutawindaya-coder <54474247+falahsutawindaya-coder@users.noreply.github.com>
2019-11-25 00:48:20 +0700

    komen

--- src/adt/graph.h
+++ src/adt/graph.h
@@ -8,7 +8,14 @@ typedef struct _Graph {
 } Graph;
 
 Graph new_Graph();
+/*I.S Defisini graph belum terdifinisi 
+  F.S Graph terdefinisi
+*/
 void Graph_add1(Graph* p, int i, int j);
+/*I.S Graph sudah ada, elemen terdefinisi
+F.S graph baru dengan edge baru i dan j */
 void Graph_add2(Graph* p, int i, int j);
+/*I.S Graph sudah ada, elemen terdefinisi
+F.S graph baru dengan edge baru dengan node i dan j dan sebalik nya dari j ke i*/
 
 #endif // __graph_h__
