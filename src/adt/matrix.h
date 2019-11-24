ed3c59487d07c477cfa8d8ddec9926f876429f4d
falahsutawindaya-coder <54474247+falahsutawindaya-coder@users.noreply.github.com>
2019-11-25 00:51:52 +0700

    komen

--- src/adt/matrix.h
+++ src/adt/matrix.h
@@ -9,5 +9,7 @@ typedef struct _Matrix {
 } Matrix;
 
 Matrix new_Matrix(int r, int c);
+/*I.S matriks tidak terdefinisi
+F.s matriks terdefinisi dengan r dan c sebagai indeks baris dan kolom*/
 
 #endif // __matrix_h__
